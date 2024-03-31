b8e322e7df4ae25ece3c987065a92e9226bc8f89
biswa-ranjan-subudhi <biswaranjan.cuh@gmail.com>
2024-03-31 15:29:17 +0530

    Fixes done ver 2

--- src/components/landing/ProjectService.js
+++ src/components/landing/ProjectService.js
@@ -6,7 +6,6 @@ import { BiLogoPlayStore } from "react-icons/bi";
 import { SiAppstore } from "react-icons/si";
 import { AiOutlineGithub } from "react-icons/ai";
 import Flex from "components/common/Flex";
-import { useMediaQuery, useTheme } from "@mui/material";
 import "../../CSS/GradientText.css";
 import { Tilt } from "react-tilt";
 import { motion } from "framer-motion";
@@ -20,169 +19,98 @@ const ProjectService = ({
   techStack,
   appstoreURL,
   playstoreURL,
+  ongoing,
 }) => {
-  const theme = useTheme();
-  const isMatch = useMediaQuery(theme.breakpoints.down("lg"));
-
   return (
-    <>
-      {isMatch ? (
-        <Card className="mb-3">
-          <Card.Img src={thumbnail} alt={title} variant="top" />
-          <Card.Body>
-            <div className="mb-3 ">
-              {techStack.map((ele, idx) => (
-                <SoftBadge
-                  key={ele.id + "-" + idx}
-                  pill
-                  bg="info"
-                  className="fs--2 me-2 text-wrap text-start"
-                >
-                  {ele.title}
-                </SoftBadge>
-              ))}
-            </div>
-            <Card.Title className="text-black">{title}</Card.Title>
-            <Card.Text className="fs--1 gray1">{subTitle}</Card.Text>
-            <div className="mt-2">
-              <Flex className="gap-3 align-items-center">
-                {liveUrl && (
-                  <Button
-                    as="a"
-                    target="_blank"
-                    href={liveUrl}
-                    variant="falcon-default"
-                    type="button"
-                    size="sm"
-                    className="icon-item icon-item-lg fs-2"
-                  >
-                    <BsFillLaptopFill className="success" />
-                  </Button>
-                )}
-                {githubUrl && (
-                  <Button
-                    as="a"
-                    target="_blank"
-                    href={githubUrl}
-                    variant="falcon-default"
-                    type="button"
-                    size="sm"
-                    className="icon-item icon-item-lg fs-2"
-                  >
-                    <AiOutlineGithub className="text-github" />
-                  </Button>
-                )}
-                {appstoreURL && (
-                  <Button
-                    as="a"
-                    target="_blank"
-                    href={appstoreURL}
-                    variant="falcon-default"
-                    type="button"
-                    size="sm"
-                    className="icon-item icon-item-lg fs-2"
-                  >
-                    <SiAppstore className="text-facebook" />
-                  </Button>
-                )}
-                {playstoreURL && (
-                  <Button
-                    as="a"
-                    target="_blank"
-                    href={playstoreURL}
-                    variant="falcon-default"
-                    type="button"
-                    size="sm"
-                    className="icon-item icon-item-lg fs-2"
-                  >
-                    <BiLogoPlayStore className="success" />
-                  </Button>
-                )}
-              </Flex>
-            </div>
-          </Card.Body>
-        </Card>
-      ) : (
-        <Card>
-          <Card.Img src={thumbnail} alt={title} variant="top" />
-
-          <Card.Body>
-            <div className="mb-3 ">
-              {techStack.map((ele, idx) => (
-                <SoftBadge
-                  key={ele.id + "-" + idx}
-                  pill
-                  bg="info"
-                  className="fs--2 me-2 text-wrap text-start"
-                >
-                  {ele.title}
-                </SoftBadge>
-              ))}
-            </div>
-            <Card.Title className="text-black justify-content-between">
-              {title}
-            </Card.Title>
-            <Card.Text className="fs--1 gray1">{subTitle}</Card.Text>
-            <div className="mt-2">
-              <Flex className="gap-3 align-items-center">
-                {liveUrl && (
-                  <Button
-                    as="a"
-                    target="_blank"
-                    href={liveUrl}
-                    variant="falcon-default"
-                    type="button"
-                    size="sm"
-                    className="icon-item icon-item-lg fs-2"
-                  >
-                    <BsFillLaptopFill className="success" />
-                  </Button>
-                )}
-                {githubUrl && (
-                  <Button
-                    as="a"
-                    target="_blank"
-                    href={githubUrl}
-                    variant="falcon-default"
-                    type="button"
-                    size="sm"
-                    className="icon-item icon-item-lg fs-2"
-                  >
-                    <AiOutlineGithub className="text-github" />
-                  </Button>
-                )}
-                {appstoreURL && (
-                  <Button
-                    as="a"
-                    target="_blank"
-                    href={appstoreURL}
-                    variant="falcon-default"
-                    type="button"
-                    size="sm"
-                    className="icon-item icon-item-lg fs-2"
-                  >
-                    <SiAppstore className="info" />
-                  </Button>
-                )}
-                {playstoreURL && (
-                  <Button
-                    as="a"
-                    target="_blank"
-                    href={playstoreURL}
-                    variant="falcon-default"
-                    type="button"
-                    size="sm"
-                    className="icon-item icon-item-lg fs-2"
-                  >
-                    <BiLogoPlayStore className="success" />
-                  </Button>
-                )}
-              </Flex>
-            </div>
-          </Card.Body>
-        </Card>
-      )}
-    </>
+    <Card>
+      <Card.Img src={thumbnail} alt={title} variant="top" />
+      <Card.Body>
+        <div className="mb-3 ">
+          {techStack.map((ele, idx) => (
+            <SoftBadge
+              key={ele.id + "-" + idx}
+              pill
+              bg="info"
+              className="me-2 text-wrap text-start"
+            >
+              {ele.title}
+            </SoftBadge>
+          ))}
+        </div>
+        <Card.Title
+          className="text-black justify-content-between"
+          style={{ fontSize: "20px" }}
+        >
+          {title}
+          <SoftBadge
+            pill
+            bg={ongoing ? "primary" : "success"}
+            className="fs--2 ms-2 text-wrap text-start"
+          >
+            {ongoing ? "In progress" : "Closed"}
+          </SoftBadge>
+        </Card.Title>
+        <Card.Text style={{ fontSize: "15px" }} className="gray1 mt-2">
+          {subTitle}
+        </Card.Text>
+        <div className="mt-2">
+          <Flex className="gap-3 align-items-center">
+            {liveUrl && (
+              <Button
+                as="a"
+                target="_blank"
+                href={liveUrl}
+                variant="falcon-default"
+                type="button"
+                size="sm"
+                className="icon-item icon-item-lg fs-2"
+              >
+                <BsFillLaptopFill className="success" />
+              </Button>
+            )}
+            {githubUrl && (
+              <Button
+                as="a"
+                target="_blank"
+                href={githubUrl}
+                variant="falcon-default"
+                type="button"
+                size="sm"
+                className="icon-item icon-item-lg fs-2"
+              >
+                <AiOutlineGithub className="text-github" />
+              </Button>
+            )}
+            {appstoreURL && (
+              <Button
+                as="a"
+                target="_blank"
+                href={appstoreURL}
+                variant="falcon-default"
+                type="button"
+                size="sm"
+                className="icon-item icon-item-lg fs-2"
+              >
+                <SiAppstore className="info" />
+              </Button>
+            )}
+            {playstoreURL && (
+              <Button
+                as="a"
+                target="_blank"
+                href={playstoreURL}
+                variant="falcon-default"
+                type="button"
+                size="sm"
+                className="icon-item icon-item-lg fs-2"
+              >
+                <BiLogoPlayStore className="success" />
+              </Button>
+            )}
+          </Flex>
+        </div>
+      </Card.Body>
+    </Card>
   );
 };
 
